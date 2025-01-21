ec21f4b08dc38c9fa3871ff15ca492f1851d2a4f
ruran8wa <p.ruran8wa@alustudent.com>
2025-01-21 08:27:01 +0200

    done

--- matrix_mul.py
+++ matrix_mul.py
@@ -1,8 +1,8 @@
-import pandas as pd
-import numpy as np
+# import pandas as pd
+# import numpy as np
 
 # Load data
-data = pd.read_csv('path/to/data')
+# data = pd.read_csv('path/to/data')
 
 # Example arrays
 Prices = [[300, 500],
@@ -16,9 +16,8 @@ Ans = []
 
 for i in range(len(Prices)):
     row_sum = 0
-    for j in range(len(Prices[0])):
-        # COMPLETE THE MISSING LOGIC HERE
-        pass
-
-
+    for j in range(len(Prices[i])):
+        row_sum += Prices[i][j] * Array2[j]
+    Ans.append(row_sum) 
 print(Ans)
+
